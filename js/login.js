0cda65d1a36b287d144509abace8e1d150ccba96
Alexisrdz1219 <ealexisrdz19@gmail.com>
2026-02-22 16:16:57 -0600

    DWd

---
+++ js/login.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  try {
+    const response = await fetch(
+      "https://buscador-refaccionesbackend.onrender.com/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "Error en login");
+    }
+
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("rol", data.rol);
+    localStorage.setItem("nombre", data.nombre);
+
+    alert("Login exitoso 🚀");
+    window.location.href = "Nadd.html";
+
+  } catch (err) {
+    alert(err.message);
+  }
+});
